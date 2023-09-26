379f7ee7199c2dd80fbef73f3ca3181b851e8693
gnuhub <gnuhub@gmail.com>
2023-09-26 16:37:32 +0800

    Update install.sh

--- ci/1020-docker-build/files/install.sh
+++ ci/1020-docker-build/files/install.sh
@@ -11,47 +11,7 @@ env
 whoami
 pwd
 
-yum update -y
-yum install epel-release -y
-yum install dnf -y
-dnf --assumeyes update
 
-dnf --assumeyes install perl 
-dnf --assumeyes install rsync
-
-rsync -avzP ./root/ /
-
-
-dnf clean all
-dnf makecache
-
-dnf --assumeyes install epel-release dnf-plugins-core
-dnf --assumeyes upgrade epel-release
-
-sed -e 's!^metalink=!#metalink=!g' \
-    -e 's!^#baseurl=!baseurl=!g' \
-    -e 's!https\?://download\.fedoraproject\.org/pub/epel!https://mirrors.tuna.tsinghua.edu.cn/epel!g' \
-    -e 's!https\?://download\.example/pub/epel!https://mirrors.tuna.tsinghua.edu.cn/epel!g' \
-    -i /etc/yum.repos.d/epel*.repo
-
-# crb enable
-
-dnf --assumeyes update
-
-dnf --assumeyes install kiwi 
-dnf --assumeyes install sudo
-dnf --assumeyes install qemu-img
-dnf --assumeyes install dosfstools
-dnf --assumeyes install git
-dnf --assumeyes install expect
-dnf --assumeyes install vim
-dnf --assumeyes openssh-server
-dnf --assumeyes groupinstall "Development Tools"
-
-
-dnf --assumeyes update
-dnf clean all
-dnf makecache
 
 ######## create the users www and runner=======
 cat /etc/group
@@ -59,8 +19,8 @@ cat /etc/group
 groupadd www
 groupadd runner
 
-useradd -m -d /home/www -G wheel -g www www -s /bin/bash
-useradd -m -d /home/runner -G wheel -g runner runner -s /bin/bash
+useradd -m -d /home/www -G sudo -g www www -s /bin/bash
+useradd -m -d /home/runner -G sudo -g runner runner -s /bin/bash
 
 echo "root:openos365" | chpasswd
 echo "runner:openos365" | chpasswd
@@ -79,20 +39,27 @@ cat /etc/passwd
 ###############################################
 
 
+apt update -y
+apt upgrade -y 
 
-git config --global pull.rebase false
-git config --global core.editor "vim"
+ln -sf "/usr/share/zoneinfo/Asia/Shanghai" "/etc/localtime"
+apt install git -y
+apt install build-essential -y 
+apt install rename -y
+apt install expect -y
+apt install curl -y
+apt install wget -y
+apt install rsync -y
+apt install python3 -y
+apt install python2 -y
+apt install sudo -y
+apt install sqlite3 -y
+apt install jq -y
+apt install yq -y
+apt install ruby -y
+apt clean
 
-cd ~
-if [ -d versions ];then
-    rm -rf versions
-fi
-mkdir versions
-cd versions
-dnf list installed > dnf.list.installed.txt
-dnf list > dnf.list.txt
 
-sed -i '1,2d' dnf.list.installed.txt
-sed -i '1d' dnf.list.txt
+
 
 
