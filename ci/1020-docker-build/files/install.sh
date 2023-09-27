47822299dcf329ef8f8b23e17010dd3cba0ddde9
gnuhub <gnuhub@gmail.com>
2023-09-27 20:39:57 +0800

    Update install.sh

--- ci/1020-docker-build/files/install.sh
+++ ci/1020-docker-build/files/install.sh
@@ -65,6 +65,10 @@ curl -LO https://storage.googleapis.com/container-diff/latest/container-diff-lin
 install container-diff-linux-amd64 /usr/bin/container-diff
 rm -rf container-diff-linux-amd64
 
+export DIVE_VERSION=$(curl -sL "https://api.github.com/repos/wagoodman/dive/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
+curl -OL https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_amd64.deb
+sudo apt install ./dive_${DIVE_VERSION}_linux_amd64.deb
+
 curl -fsSL https://code-server.dev/install.sh | sh
 
 systemctl enable code-server@root
