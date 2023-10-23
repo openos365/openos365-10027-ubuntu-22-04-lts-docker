d6ecdc6458078332ab8f9fde44deecaad2b68a1d
gnuhub <gnuhub@gmail.com>
2023-10-24 07:20:27 +0800

    Update install.sh

--- ci/1020-docker-build/files/install.sh
+++ ci/1020-docker-build/files/install.sh
@@ -161,6 +161,14 @@ curl -fsSL https://code-server.dev/install.sh | sh
 
 systemctl enable code-server@www
 
+# https://github.com/cli/cli/blob/trunk/docs/install_linux.md
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
 cd ~/
 git clone https://github.com/gnuhub/connect-proxy.git
 cd connect-proxy
