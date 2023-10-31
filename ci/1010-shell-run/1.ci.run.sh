7ff991d2b2c8eb64d6c6559baabfc7dcddd63912
gnuhub <gnuhub@qq.com>
2023-10-31 12:51:32 +0800

    clean

--- ci/1010-shell-run/1.ci.run.sh
+++ ci/1010-shell-run/1.ci.run.sh
@@ -10,5 +10,21 @@ echo "==========================================================================
 # TODO HERE
 
 cat /proc/version_signature
+sudo df -h
+
+# https://github.com/jlumbroso/free-disk-space/blob/main/action.yml
+sudo rm -rf /opt/ghc
+
+sudo apt-get remove -y '^dotnet-.*'
+sudo apt-get remove -y '^llvm-.*'
+sudo apt-get remove -y 'php.*'
+sudo apt-get remove -y '^mongodb-.*'
+sudo apt-get remove -y '^mysql-.*'
+sudo apt-get remove -y azure-cli google-cloud-sdk google-chrome-stable firefox powershell mono-devel libgl1-mesa-dri
+sudo apt-get autoremove -y
+sudo apt-get clean
+echo "$AGENT_TOOLSDIRECTORY"
+sudo rm -rf "$AGENT_TOOLSDIRECTORY"
+sudo df -h
 
 echo "============================================================================"
